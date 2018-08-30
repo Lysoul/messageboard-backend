b85d0a9c025aea0f0ba70b66ef348e1b9cb62c51
Jakkavat (Bank) Deemak <jd@manaosoftware.com>
2018-08-30 14:40:04 +0700

    Add message controler.

---
+++ MessageBoardBackend/Controllers/MessagesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessageBoardBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        public IEnumerable<Models.Message> Get()
+        {
+            return new Models.Message[]
+            {
+                new Models.Message
+                {
+                    Owner = "Jakkavat",
+                    Text = "Heloo Bank!"
+                },
+                new Models.Message
+                {
+                    Owner = "John",
+                    Text = "Heloo John!"
+                }
+            };
+
+        }
+    }
+}
